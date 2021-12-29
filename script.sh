cec0c5adfc1e8061d35aa22227bbbc00814df7bc
Sarfo Kantanka Frimpong <frimpongsarfok@gmail.com>
2021-12-29 00:39:16 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,13 @@ CERTIFICATE_KEY_FILE_NAME="Apple Push Sandox Cert Key"
 TOPIC="App ID"
 DEVICE_TOKEN="<device token>"
 APNS_HOST_NAME=api.sandbox.push.apple.com
-curl -v --header "apns-topic: ${TOPIC}" --header "apns-push-type: alert" --cert "${CERTIFICATE_FILE_NAME}" --cert-type PEM --key "${CERTIFICATE_KEY_FILE_NAME}" --key-type PEM --data '{"aps":{"alert":"test"}}' --http2  https://${APNS_HOST_NAME}/3/device/${DEVICE_TOKEN}
+curl -v --header "apns-topic: ${TOPIC}" \
+--header "apns-push-type: alert" \
+--cert "${CERTIFICATE_FILE_NAME}"\
+--cert-type PEM \
+--key "${CERTIFICATE_KEY_FILE_NAME}" \
+--key-type PEM --data '{"aps":{"alert":"test"}}' \
+--http2  https://${APNS_HOST_NAME}/3/device/${DEVICE_TOKEN}
 
 #Production
 CERTIFICATE_FILE_NAME="Apple Push Prod. Cert"
@@ -16,4 +22,10 @@ CERTIFICATE_KEY_FILE_NAME="Apple Push Prod. Cert Key"
 TOPIC="App ID"
 DEVICE_TOKEN="<device token>"
 APNS_HOST_NAME=api.push.apple.com
-curl -v --header "apns-topic: ${TOPIC}" --header "apns-push-type: alert" --cert "${CERTIFICATE_FILE_NAME}" --cert-type PEM --key "${CERTIFICATE_KEY_FILE_NAME}" --key-type PEM --data '{"aps":{"alert":"test"}}' --http2  https://${APNS_HOST_NAME}/3/device/${DEVICE_TOKEN}
+curl -v --header "apns-topic: ${TOPIC}" \
+--header "apns-push-type: alert" \
+--cert "${CERTIFICATE_FILE_NAME}" \
+--cert-type PEM \
+--key "${CERTIFICATE_KEY_FILE_NAME}" \
+--key-type PEM --data '{"aps":{"alert":"test"}}' \
+--http2  https://${APNS_HOST_NAME}/3/device/${DEVICE_TOKEN}
